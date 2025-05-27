919935d4a637e56dab54d725b9a32afdb5001b29
adomingues17 <adomin0111@gmail.com>
2025-05-26 22:42:59 -0300

    v1 - Lançamento

---
+++ Arquivos/Aula13Arquivos/Program.cs
@@ -0,0 +1,56 @@
+﻿using Aula13Arquivos.Services;
+
+namespace Aula13Arquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunApplication();
+        }
+        
+        public static void RunApplication()
+        {
+            while (true)
+            {
+                DisplayMenu();
+                var opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o nome do diretório, por favor.");
+                        string arquivo = Console.ReadLine();
+                        FileDataService.CriarArquivo(arquivo);                     
+                        break;
+                    case "2":
+                        FileDataService.LerArquivo();                     
+                        break;
+                    case "3":
+                        FileDataService.AcrescentarTexto();                        
+                        break;
+                    case "4":
+                        FileDataService.ApagarArquivo();                        
+                        break;
+                    case "5":
+                        return;                       
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+                Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+        }        
+        private static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Criar Arquivo.");
+            Console.WriteLine("2 - Ler Arquivo");
+            Console.WriteLine("3 - Acrescentar Texto.");
+            Console.WriteLine("4 - Apagar Arquivo.");
+            Console.WriteLine("5 - Sair");
+        }
+    }
+}
---
+++ Arquivos/Aula13Arquivos/Services/FileDataService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Aula13Arquivos.Services
+{
+    public class FileDataService()
+    {
+        public static void CriarArquivo(string nomeDiretorio) {            
+            string path = @"D:\impacta\4cSharp\arquivos\";
+            string pathComplete = Path.Combine(path, nomeDiretorio);
+            Directory.CreateDirectory(pathComplete);
+            Console.WriteLine("Diretório criado.");
+            Console.ReadKey();
+        }
+
+        public static void LerArquivo() {
+            string path = @"D:\impacta\4cSharp\arquivos\aula13.txt";
+            string texto = File.ReadAllText(path);
+            Console.WriteLine(texto);
+            Console.ReadKey();
+        }
+
+        public static void AcrescentarTexto() {
+            string path = @"D:\impacta\4cSharp\arquivos\aula13b.txt";
+            string texto = "\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, \n" +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n" +
+                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip \n" +
+                "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit \n" +
+                "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, \n" +
+                "sunt in culpa qui officia deserunt mollit anim id est laborum. \n";
+            File.AppendAllText(path, texto);
+            Console.WriteLine(texto);
+            Console.ReadKey();
+        }
+
+        public static void ApagarArquivo() {
+            string path = @"D:\impacta\4cSharp\arquivos\novaPasta";
+            Directory.Delete(path);
+            Console.WriteLine("Diretório deletado.");
+            Console.ReadKey();
+        }
+    }
+}
